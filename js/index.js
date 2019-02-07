9e8b73c6327220796d39d5dbc9266d877edc94f6
Siddharth <siddharthpadwal3@gmail.com>
2019-02-08 02:25:33 +0530

    fun project

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.element', {
+    strings: ["I Love You The Most in the World..", "..Because You Just Do Not Mean To Me As A Gf But As My Life Saviour!", "So Please Bare With Me For Rest Of Your Life ","Abhi Toh Gussa Thanda Kar De Please!!"],
+    typeSpeed: 125
+});
+
+$('.carousel').carousel();
